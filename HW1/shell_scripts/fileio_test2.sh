d82396d556f0a1655fc468be7fbc3d0441382c53
Zelu Liang <zeluliang@gmail.com>
2022-10-15 15:59:34 -0700

    update shell scripts

--- HW1/shell_scripts/fileio_test2.sh
+++ HW1/shell_scripts/fileio_test2.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
-for(i=0;i<5;i++)
+for((i=0;i<5;i++))
 do
     sysbench --num-threads=16 --test=fileio --file-total-size=2G --file-test-mode=seqwr prepare
     sysbench --num-threads=16 --test=fileio --file-total-size=2G --file-test-mode=seqwr run
--- HW1/sysbench_script.sh
+++ HW1/sysbench_script.sh
@@ -2,7 +2,7 @@
 
 chmod +777 ./shell_scripts/*.sh
 
-for(i=1;i<4;i++)
+for((i=1;i<4;i++))
 do
     ./shell_script/cpu_test$i.sh > ./results/test_cpu$i.txt
     ./shell_script/fileio_test$i.sh > ./results/fileio_cpu$i.txt
